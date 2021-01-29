0f716d97d7a962abaafae92d411a9690c8473166
andres segovia muñoz <andres.segmu@gmail.com>
2021-01-29 14:06:40 +0100

    SuperCommit

---
+++ GolfGame/Assets/Scripts/Game/BallHighButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BallHighButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private BallController ballController = null;
+    [SerializeField] private int direction = 1;
+    [SerializeField] private float HighSpeed = 30;
+    private bool ChangeHigh;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ChangeHigh = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        ChangeHigh = false;
+    }
+
+    private void Update()
+    {
+        if (ChangeHigh)
+        {
+            if(ballController.BallHigh <= 25 && ballController.BallHigh >= 1)
+                ballController.BallHigh += HighSpeed * direction * Time.deltaTime;
+        }
+    }
+}
